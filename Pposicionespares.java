ad30a8ee2b39277b6b0e47a8f0922d25e8bb3599
Minorusal <minorusal@hotmail.com>
2024-12-10 10:34:20 -0600

    Se solicita ingresar 10 números se usa el bucle for para iterar 10 veces, usamos scanner.hasNextInt para verificar si es un número entero valido, si no es valida con scanner.next() descarta la entrada y por ultimo con if (i % 2 ==0)verifica siel índice es par

--- Pposicionespares.java
+++ Pposicionespares.java
@@ -6,5 +6,22 @@ public class Pposicionespares {
         int[] numeros = new int[10];
         int sumaPares = 0, countPares = 0;
 
+          System.out.println("Ingrese 10 números enteros:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            while (!scanner.hasNextInt()) { 
+                System.out.print("Entrada no válida. Intente nuevamente: ");
+                scanner.next();
+            }
+            numeros[i] = scanner.nextInt();
+
+            if (i % 2 == 0) { 
+                sumaPares += numeros[i];
+                countPares++;
+            }
+        }
+
+
+       
     }
 }    
