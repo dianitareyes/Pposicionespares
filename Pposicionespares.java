79af7d6b61675cab0cfe0fb737e682eb08df12eb
Minorusal <minorusal@hotmail.com>
2024-12-10 10:26:05 -0600

    se agregan variables y se declaran

--- Pposicionespares.java
+++ Pposicionespares.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Pposicionespares {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+        int sumaPares = 0, countPares = 0;
+
     }
 }    
